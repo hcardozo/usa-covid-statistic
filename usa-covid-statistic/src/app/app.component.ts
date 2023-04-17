12e7734f6600c8faf1c866cdba86df8925d6fffa
Harold <harolduvan99@gmail.com>
2023-04-16 23:54:06 -0500

    feat(): initial commit.

---
+++ usa-covid-statistic/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { NgbCalendar, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'usa-covid-statistic';
+  submitted = false;
+  fecha: any = {}; // objeto para almacenar el modelo de datos
+  datosObtenidos: any;
+
+  constructor(public httpService: HttpClient) {
+    // inicializar el modelo con la fecha actual
+  }
+
+  public searchInfo() {
+    console.log(this.fecha.replace(/-/g, ''));
+
+    this.httpService.get(`https://api.covidtracking.com/v1/us/${this.fecha.replace(/-/g, '')}.json`).subscribe((response:any) =>{
+      if (response?.error) {
+        alert('Datos no encontrados, seleccione una fecha mas antigua.')
+      }
+    }, (errorResponse)=>{
+      if (errorResponse?.error) {
+        alert('Datos no encontrados, seleccione una fecha mas antigua.')
+      }
+    });
+
+  }
+
+
+}
